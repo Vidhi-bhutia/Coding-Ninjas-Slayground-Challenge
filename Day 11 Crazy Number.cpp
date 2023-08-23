f2095c60460a213212f20cdfb1642e793617f83b
Vidhi Bhutia <139327460+Vidhi-bhutia@users.noreply.github.com>
2023-08-23 11:53:36 +0530

    Create Day 11 Crazy Number.cpp

---
+++ Day 11 Crazy Number.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h> 
+vector < vector < int > > numberPattern(int n) {
+  vector < vector < int > >ans;
+  int num = 1;
+  for(int row=1;row<=n;row++)
+  {
+    int cnt = 1;
+    vector<int>temp(n,-1);
+    while(cnt<=row)
+    {
+      temp[n-row+cnt-1] = num;
+      num++,cnt++;
+      if(num == 10)num = 1;
+    }
+    ans.push_back(temp);
+  }
+  return ans;
+}
